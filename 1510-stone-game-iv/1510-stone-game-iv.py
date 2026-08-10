9c9b7137305fdd87ee13d3cbbfab6760b92ba02c
Aanya <154660363+aanya17@users.noreply.github.com>
2026-08-10 19:40:22 +0530

    Time: 2395 ms (11.82%), Space: 20.1 MB (67.88%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.py
+++ 1510-stone-game-iv/1510-stone-game-iv.py
@@ -5,11 +5,25 @@ class Solution:
         #     return True
         # return False
         dp = [False] * (n+1)
-        for i in range(1, n+1):
+        for i in range(1,n+1):
             j = 1
             while j*j <= i:
                 if not dp[i - j * j]:
                     dp[i] = True
-                    break
                 j += 1
         return dp[n]
+# import math
+# class Solution:
+#     def winnerSquareGame(self, n: int) -> bool:
+#         # if n == 1 or math.sqrt(n).is_integer():
+#         #     return True
+#         # return False
+#         dp = [False] * (n+1)
+#         for i in range(1, n+1):
+#             j = 1
+#             while j*j <= i:
+#                 if not dp[i - j * j]:
+#                     dp[i] = True
+#                     break
+#                 j += 1
+#         return dp[n]
